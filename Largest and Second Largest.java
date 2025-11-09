76fb639a4494d45a1208a25f4d5aa366673d7561
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2025-11-09 14:22:04 +0530

    Add Largest and Second Largest finder
    Implement a program to find the largest and second largest numbers in an array.

---
+++ Largest and Second Largest.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long a[] = new long[n];
+        long c[] = new long[n];
+        int d =0;
+        int b =0;
+        for(int i=0;i<n;i++)
+            a[i] = sc.nextLong();
+        long max = a[0];
+        for(int i =1;i<n;i++){
+            if(a[i]>max){
+                max = a[i];
+                 b = i;
+            }
+        }
+        for(int i = 0;i<n;i++){
+            if(i!=b){
+                c[i] = max - a[i];
+                
+            }
+        }
+        long min = c[0];
+        for(int i=0;i<n;i++){
+            if(i!=b){ 
+                if(c[i]<min){
+                    min= c[i];
+                    d = i;
+                } 
+            }   
+        }
+        long s = a[d]; 
+        System.out.print(max+" "+s);
+    }
+}
